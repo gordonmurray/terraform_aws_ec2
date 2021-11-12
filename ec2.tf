515832fce493f5da0bd9356925b361230fcd1620
Gordon Murray <gordon@gordonmurray.com>
2021-11-12 21:37:39 +0000

    Enable termination protection

--- ec2.tf
+++ ec2.tf
@@ -16,11 +16,12 @@ data "aws_ami" "ubuntu" {
 
 # Create EC2 instance
 resource "aws_instance" "example" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t3.micro"
-  key_name               = aws_key_pair.pem-key.key_name
-  subnet_id              = aws_subnet.subnet-1a.id
-  vpc_security_group_ids = [aws_security_group.example.id]
+  ami                     = data.aws_ami.ubuntu.id
+  instance_type           = "t3.micro"
+  key_name                = aws_key_pair.pem-key.key_name
+  subnet_id               = aws_subnet.subnet-1a.id
+  vpc_security_group_ids  = [aws_security_group.example.id]
+  disable_api_termination = true
 
   root_block_device {
     delete_on_termination = true
