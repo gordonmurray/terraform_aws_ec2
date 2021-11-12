dc7bbeef9ac63b68d2df70406e6798f3c0a0bcb9
Gordon Murray <gordon@gordonmurray.com>
2021-11-12 21:12:24 +0000

    Ignore properly

--- security_group_rules.tf
+++ security_group_rules.tf
@@ -23,7 +23,7 @@ resource "aws_security_group_rule" "example_egress" {
   from_port         = 0
   to_port           = 0
   protocol          = "all"
-  cidr_blocks       = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-ingress-sgr
+  cidr_blocks       = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-egress-sgr
   security_group_id = aws_security_group.example.id
   description       = "Allow all out"
 }
